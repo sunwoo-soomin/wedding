df3e00bfd125ac133c1a431f390ae88fe09946c6
Sunwoo <psunu0306@gmail.com>
2024-06-03 22:14:31 +0900

    1차 빌드테스트

---
+++ src/views/Card.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <h2>선우와 수민이 모바일 청첩장 만들기 우당탕탕 대작전</h2>
+  <img class="soomin 01" src="../assets/images/soomin_01.jpeg" />
+  <img class="sunwoo 01" src="../assets/images/sunwoo_01.jpeg" />
+</template>
+<style>
+.soomin {
+  width: 300px;
+}
+.sunwoo {
+  width: 314px;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import Card from '../views/Card.vue'
+</script>
+<template>
+  <Card />
+</template>
